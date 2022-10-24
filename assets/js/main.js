994772cdb55e52e71c43372114afb7e744f3c189
Andrey <duhovlovec@gmail.com>
2022-10-24 21:53:12 +0700

    adding project in mind and testimonail

--- assets/js/main.js
+++ assets/js/main.js
@@ -97,13 +97,15 @@ modalCLoses.forEach(modalClose => {
 })
 
 /*==================== PORTFOLIO SWIPER  ====================*/
-let swiper = new Swiper(".portfolio__container", {
+let swiperPortfolio = new Swiper(".portfolio__container", {
     cssMode: true,
     loop: true,
+
     navigation: {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
     },
+
     pagination: {
       el: ".swiper-pagination",
       clickable: true,
@@ -111,7 +113,24 @@ let swiper = new Swiper(".portfolio__container", {
   });
 
 /*==================== TESTIMONIAL ====================*/
+let swiperTestimonail = new Swiper(".testimonial__container", {
+    loop: true,
+    grabCursor: true,
+    spaceBetween: 48,
 
+
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: true,
+    },
+    breakpoints:{
+        568:{
+            slidesPerView: 2,
+        }
+    }
+
+  });
 
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
 
